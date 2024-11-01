74eb47f93e1a629198b78179e8c611294b879608
Erik Štvrtecký <estvrtecky@gmail.com>
2024-11-01 11:41:31 +0100

    Implement simple color recognition functionality

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import cv2
+from color_recognition import ColorRecognition
 
 
 def main():
+    lower_red = [0, 100, 100]
+    upper_red = [10, 255, 255]
+    cr = ColorRecognition(lower_red, upper_red)
     cap = cv2.VideoCapture(0)
 
     while cap.isOpened():
@@ -10,6 +14,9 @@ def main():
             break
 
         cv2.imshow("Webcam", frame)
+
+        processed_frame = cr.detect_color(frame)
+        cv2.imshow("Frame with Contours", processed_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
